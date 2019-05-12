e31f8db753b7d9109d7ba19a9327d051021cda56
extsui <en422641@s.okayama-u.ac.jp>
2019-05-12 15:48:21 +0900

    [update] M5Stackボタンで輝度UP/DOWN・停止する機能を追加

--- KawaiiMakeMYBoard/KawaiiMakeMYBoard.ino
+++ KawaiiMakeMYBoard/KawaiiMakeMYBoard.ino
@@ -29,13 +29,82 @@ void setup()
   grass_front->config(1);
   grass_middle->config(1);
   tree->config(1);
+
+  // ボタン初期化
+  pinMode(BUTTON_A_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_B_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_C_PIN, INPUT_PULLUP);
+
   Serial.print("OK.\n");
+}
+
+void step(void)
+{
+  land->next();
+  grass_front->next();
+  grass_middle->next();
+  tree->next();
+
+  land->update();
+  grass_front->update();
+  grass_middle->update();
+  tree->update();
+}
+
+void setBrightness(uint8_t brightness)
+{
+  land->config(brightness);
+  grass_front->config(brightness);
+  grass_middle->config(brightness);
+  tree->config(brightness);
 }
 
 void loop()
 {
-  land->test();
-  //grass_front->test();
-  //grass_middle->test();
-  //tree->test();
+  static uint32_t next_update_time_ms = 0;
+  uint32_t current_time_ms;
+  
+  current_time_ms = millis();
+
+  // 一定周期でエフェクト更新
+  if (next_update_time_ms <= current_time_ms) {
+    next_update_time_ms += 5000;
+
+    land->set((LandPattern)random(0, LAND_PATTERN_NUM));
+    grass_front->set((GrassPattern)random(0, GRASS_PATTERN_NUM));
+    grass_middle->set((GrassPattern)random(0, GRASS_PATTERN_NUM));
+    tree->set((TreePattern)random(0, TREE_PATTERN_NUM));
+  } else {
+    step();
+  }
+
+  static int brightness = 1;
+
+  // ボタンAで輝度DOWN
+  if (M5.BtnA.isPressed()) {
+    if (brightness >= 1) {
+      brightness--;
+    }
+  }
+
+  // ボタンBが押されている間更新停止
+  if (M5.BtnB.isPressed()) {
+    while (M5.BtnB.isPressed()) {
+      M5.update();
+    }
+  }
+
+  // ボタンCで輝度UP
+  if (M5.BtnC.isPressed()) {
+    if (brightness < 15) {
+      brightness++;
+    }
+  }
+
+  setBrightness(brightness);
+
+  // ボタン情報更新
+  M5.update();
+
+  delay(50);
 }
