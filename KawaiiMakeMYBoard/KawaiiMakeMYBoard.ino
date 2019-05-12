0623fd79ee7b88f9df7d13ab48b6183705d71a36
extsui <en422641@s.okayama-u.ac.jp>
2019-05-13 00:17:43 +0900

    [update] 画面表示(バージョン、現在のパターン)を作成

--- KawaiiMakeMYBoard/KawaiiMakeMYBoard.ino
+++ KawaiiMakeMYBoard/KawaiiMakeMYBoard.ino
@@ -5,6 +5,8 @@
 #include "Grass.h"
 #include "Land.h"
 
+#define KMMB_VERSION  "v0.1"
+
 Land *land;
 Grass *grass_front;
 Grass *grass_middle;
@@ -35,6 +37,8 @@ void setup()
   pinMode(BUTTON_B_PIN, INPUT_PULLUP);
   pinMode(BUTTON_C_PIN, INPUT_PULLUP);
 
+  M5.Lcd.fillScreen(ILI9341_BLACK);
+
   Serial.print("OK.\n");
 }
 
@@ -59,6 +63,9 @@ void setBrightness(uint8_t brightness)
   tree->config(brightness);
 }
 
+// 各パーツの輝度(共通)
+static int brightness = 1;
+
 void loop()
 {
   static uint32_t next_update_time_ms = 0;
@@ -70,15 +77,21 @@ void loop()
   if (next_update_time_ms <= current_time_ms) {
     next_update_time_ms += 5000;
 
-    land->set((LandPattern)random(0, LAND_PATTERN_NUM));
-    grass_front->set((GrassPattern)random(0, GRASS_PATTERN_NUM));
-    grass_middle->set((GrassPattern)random(0, GRASS_PATTERN_NUM));
-    tree->set((TreePattern)random(0, TREE_PATTERN_NUM));
+    uint8_t patterns[4];
+    patterns[0] = random(0, LAND_PATTERN_NUM);
+    patterns[1] = random(0, GRASS_PATTERN_NUM);
+    patterns[2] = random(0, GRASS_PATTERN_NUM);
+    patterns[3] = random(0, TREE_PATTERN_NUM);
+
+    land->set((LandPattern)patterns[0]);
+    grass_front->set((GrassPattern)patterns[1]);
+    grass_middle->set((GrassPattern)patterns[2]);
+    tree->set((TreePattern)patterns[3]);
+
+    drawScreen(patterns);
   } else {
     step();
   }
-
-  static int brightness = 1;
 
   // ボタンAで輝度DOWN
   if (M5.BtnA.isPressed()) {
@@ -107,4 +120,50 @@ void loop()
   M5.update();
 
   delay(50);
+}
+
+#define TEXT_HEIGHT (16)
+
+void drawScreen(uint8_t patterns[])
+{
+  /*
+   * [画面サンプル]
+   * ------------------------------
+   * KawaiiMakeMYBoard <version>
+   *   0x70 Land  <pattern#>
+   *   0x71 Grass <pattern#>
+   *   0x72 Grass <pattern#>
+   *   0x73 Tree  <pattern#>
+   * ------------------------------
+   */
+  char str[32];
+
+  struct {
+    char *text;
+    uint16_t color;
+  } format[] = {
+    { "KawaiiMakeMYBoard %s", ILI9341_WHITE,  },
+    { "  0x70 Land  %d",      ILI9341_YELLOW, },
+    { "  0x71 Grass %d",      ILI9341_GREEN,  },
+    { "  0x72 Grass %d",      ILI9341_GREEN,  },
+    { "  0x73 Tree  %d",      ILI9341_BLUE,   },
+  };
+
+  M5.Lcd.fillScreen(ILI9341_BLACK);
+
+  M5.Lcd.setTextSize(2);
+
+  M5.Lcd.setTextColor(format[0].color);
+  M5.Lcd.setCursor(0, 0*TEXT_HEIGHT);
+  sprintf(str, format[0].text, KMMB_VERSION, brightness);
+  M5.Lcd.print(str);
+
+  for (int i = 1; i <= 4; i++) {
+    M5.Lcd.setTextColor(format[i].color);
+    M5.Lcd.setCursor(0, (i)*TEXT_HEIGHT);
+    sprintf(str, format[i].text, patterns[i-1]);
+    M5.Lcd.print(str);
+  }
+  // TODO: 家用
+  //M5.Lcd.setTextColor(ILI9341_RED);
 }
